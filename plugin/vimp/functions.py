ced0ad82d039b4b046ea8897806a746dc16d4437
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2019-11-20 23:16:53 +0900

    fixed decorator

--- plugin/vimp/functions.py
+++ plugin/vimp/functions.py
@@ -30,8 +30,8 @@ def function(name=None):
 
     def decorator(f):
         assert callable(f)
-        is_function = hasattr(f, "func_name")
-        function_name = name or (f.func_name if is_function else f.__name__)
+        is_function = True if str(f.__class__) == "<class 'function'>" else False
+        function_name = name or f.__name__
         assert function_name not in _functions
         proto = """
 function! {0}(...)
